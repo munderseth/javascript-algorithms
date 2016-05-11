e77c50c233d0bcb3e68612888ac614d04df0e725
lucasfcosta <fernandesdacostalucas@gmail.com>
2016-05-11 20:03:44 -0300

    Update project structure to support tests and coverage

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
+const path = require('path');
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 const eslint = require('gulp-eslint');
+const istanbul = require('gulp-istanbul');
+const coveralls = require('gulp-coveralls');
 const excludeGitignore = require('gulp-exclude-gitignore');
 
 gulp.task('eslint', () => {
@@ -11,8 +14,28 @@ gulp.task('eslint', () => {
         .pipe(eslint.failAfterError());
 });
 
-gulp.task('test', () => {
-    gulp.src('test/**/*.js')
-        .pipe(babel())
-        .pipe(mocha({reporter: 'spec'}));
+gulp.task('pre-test', () => {
+    return gulp.src('algorithms/**/*.js')
+        .pipe(istanbul({
+            includeUntested: true
+        }))
+        .pipe(istanbul.hookRequire());
 });
+
+gulp.task('test', ['pre-test'], () => {
+    gulp.src('test/**/*.js')
+        .pipe(mocha({reporter: 'spec'}))
+        .pipe(istanbul.writeReports())
+        .pipe(istanbul.enforceThresholds({thresholds: {global: 100}}));
+});
+
+gulp.task('coveralls', ['test'], () => {
+    if (!process.env.CI) {
+        return;
+    }
+
+    return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
+        .pipe(coveralls());
+});
+
+gulp.task('default', ['eslint', 'coveralls']);
