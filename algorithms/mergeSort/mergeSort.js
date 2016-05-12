f1728ec1aa14123350ad6e9367ad0474a249edf0
lucasfcosta <fernandesdacostalucas@gmail.com>
2016-05-11 22:21:41 -0300

    Merge sort and tests for it

---
+++ algorithms/mergeSort/mergeSort.js
@@ -0,0 +1,29 @@
+const merge = require('../merge/merge');
+
+/**
+ * ### .mergeSort(arr)
+ *
+ * Sorts an array.
+ *
+ * @name merge
+ * @param {Array} arr
+ * @returns array with ordered elements
+ */
+function mergeSort(arr) {
+    // If this is called with a single item array we can consider it's already sorted
+    if (arr.length === 1) {
+        return [arr[0]];
+    }
+
+    // Here we split the array in two
+    let middleIndex = Math.floor(arr.length/2);
+    let left = arr.slice(0, Math.floor(arr.length/2));
+    let right = arr.slice(middleIndex, arr.length);
+
+    // Now we keep splitting the parts and merging then in order
+    // When merge gets two arrays with a single elements it sorts those two
+    // Then we merge those two with another two which were already sorted and so on...
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+module.exports = mergeSort;
---
+++ test/mergeSort.js
@@ -0,0 +1,58 @@
+require('chai').Should();
+const mergeSort = require('../algorithms/mergeSort/mergeSort');
+
+describe('MergeSort', () => {
+    it('Sorts ordered arrays', () => {
+        let oneToSix = mergeSort([1, 2, 3, 4, 5, 6]);
+        oneToSix[0].should.be.equal(1);
+        oneToSix[1].should.be.equal(2);
+        oneToSix[2].should.be.equal(3);
+        oneToSix[3].should.be.equal(4);
+        oneToSix[4].should.be.equal(5);
+        oneToSix[5].should.be.equal(6);
+
+        let orderedRandomArray = mergeSort([0, 9, 18, 21, 33, 58]);
+        orderedRandomArray[0].should.be.equal(0);
+        orderedRandomArray[1].should.be.equal(9);
+        orderedRandomArray[2].should.be.equal(18);
+        orderedRandomArray[3].should.be.equal(21);
+        orderedRandomArray[4].should.be.equal(33);
+        orderedRandomArray[5].should.be.equal(58);
+    });
+
+    it('Sorts randomly ordered arrays', () => {
+        let oneToSix = mergeSort([1, 5, 3, 2, 4, 6]);
+        oneToSix[0].should.be.equal(1);
+        oneToSix[1].should.be.equal(2);
+        oneToSix[2].should.be.equal(3);
+        oneToSix[3].should.be.equal(4);
+        oneToSix[4].should.be.equal(5);
+        oneToSix[5].should.be.equal(6);
+
+        let randomlySortedArray = mergeSort([21, 18, 0, 33, 9, 58]);
+        randomlySortedArray[0].should.be.equal(0);
+        randomlySortedArray[1].should.be.equal(9);
+        randomlySortedArray[2].should.be.equal(18);
+        randomlySortedArray[3].should.be.equal(21);
+        randomlySortedArray[4].should.be.equal(33);
+        randomlySortedArray[5].should.be.equal(58);
+    });
+
+    it('Sorts reversed arrays', () => {
+        let oneToSix = mergeSort([6, 5, 4, 3, 2, 1]);
+        oneToSix[0].should.be.equal(1);
+        oneToSix[1].should.be.equal(2);
+        oneToSix[2].should.be.equal(3);
+        oneToSix[3].should.be.equal(4);
+        oneToSix[4].should.be.equal(5);
+        oneToSix[5].should.be.equal(6);
+
+        let randomlySortedArray = mergeSort([58, 33, 21, 18, 9, 0]);
+        randomlySortedArray[0].should.be.equal(0);
+        randomlySortedArray[1].should.be.equal(9);
+        randomlySortedArray[2].should.be.equal(18);
+        randomlySortedArray[3].should.be.equal(21);
+        randomlySortedArray[4].should.be.equal(33);
+        randomlySortedArray[5].should.be.equal(58);
+    });
+});
